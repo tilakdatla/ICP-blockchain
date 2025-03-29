e0fb00b0cca9e30fa9ff0b5ec24d96a8d8d2f67a
Tilak Datla <tilakdatla@gmail.com>
2025-03-29 13:03:29 +0000

    Initial commit

---
+++ src/token_assets/src/index.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./components/App";
+
+// Directly render the App component without authentication
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
